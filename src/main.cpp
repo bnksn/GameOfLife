45b916e178a9cd09b1440473c6e11172db9dfb17
bnksn <64915122+bnksn@users.noreply.github.com>
2025-01-20 04:29:36 +0000

    Simplify functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,27 @@
 
 #include "gameOfLife.hpp"
 
-void printBoard(const std::vector<std::vector<char>>& board) {
-    for (const auto& row : board) {
-        for (const auto cell : row) {
-            std::cout << cell;
+void printResults(
+    const std::vector<std::vector<std::vector<char>>>& results) {
+    for (const auto& board : results) {
+        for (const auto& row : board) {
+            for (const auto cell : row) {
+                std::cout << cell;
+            }
+            std::cout << '\n';
         }
         std::cout << '\n';
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
 
-void printRes(
-    const std::vector<std::vector<std::vector<char>>>& res) {
-    for (const auto& board : res) {
-        printBoard(board);
-        std::cout << '\n';
+[[nodiscard]]
+std::vector<std::vector<char>> createInitialBoard(
+    std::vector<std::vector<char>> centre, const int boardSize) {
+    auto initial = std::vector<std::vector<char>>(
+        boardSize, std::vector<char>(boardSize, '_'));
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-}
-
-void insertCentre(std::vector<std::vector<char>>& initial,
-                  const std::vector<std::vector<char>>& centre,
-                  const int boardSize) {
     const auto centreSize = centre.size();
     const auto topLeftIndex = (boardSize - centreSize) / 2;
 
@@ -35,23 +34,20 @@ void insertCentre(std::vector<std::vector<char>>& initial,
                 centre[i][j];
         }
     }
+
+    return initial;
 }
 
 int main() {
     constexpr auto boardSize = 20;
-    auto initial = std::vector<std::vector<char>>(
-        boardSize, std::vector<char>(boardSize, '_'));
-
-    const auto centre =
-        std::vector<std::vector<char>>{{'_', '#', '_', '_'},
-                                       {'#', '#', '#', '_'},
-                                       {'_', '#', '_', '_'},
-                                       {'_', '_', '_', '_'}};
-
-    insertCentre(initial, centre, boardSize);
+    constexpr auto numIterations = 50;
+    const auto initial = createInitialBoard({{'_', '#', '_', '_'},
+                                             {'#', '#', '#', '_'},
+                                             {'_', '#', '_', '_'},
+                                             {'_', '_', '_', '_'}},
+                                            boardSize);
 
     const auto gameOfLife = GameOfLife(boardSize);
-    constexpr auto numIterations = 50;
     const auto res = gameOfLife.runSimulation(initial, numIterations);
-    printRes(res);
+    printResults(res);
 }
